317c1eade95ce0c66443b60390d3d1cb5f937e4c
nanotechidd <118824275+nanotechidd@users.noreply.github.com>
2022-11-24 11:21:09 +0800

    Update trial.sh

--- ssh/trial.sh
+++ ssh/trial.sh
@@ -4,7 +4,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/nanotechid/supreme/aio/permission/ip > /root/tmp
+    curl -sS https://raw.githubusercontent.com/nanotechidd/supreme/aio/permission/ip > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do

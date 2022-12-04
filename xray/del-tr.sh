7098e30c3280e4ec616b45186e94b65fd8b84421
nanotechidd <118824275+nanotechidd@users.noreply.github.com>
2022-12-04 18:35:59 +0800

    Update del-tr.sh

--- xray/del-tr.sh
+++ xray/del-tr.sh
@@ -39,12 +39,13 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/nanotechidd/supreme/permission/ip | awk '{print $4}' | grep $MYIP)
-    if [ "$MYIP" = "$IZIN" ]; then
-    Bloman
-    else
-    res="Permission Denied!"
-    fi
+    IZIN=$(curl -sS https://raw.githubusercontent.com/nanotechidd/supreme/aio/permission/ip | awk '{print $4}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo -e "\e[32mPermission Accepted...\e[0m"
+else
+echo -e "\e[31mPermission Denied!\e[0m";
+exit 0
+fi
     BURIQ
 }
 red='\e[1;31m'
